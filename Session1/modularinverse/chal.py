d6f96a75ddc38d882b783f950d07ae4caf833132
l0mb4rd <125534008+ahmdobeidat@users.noreply.github.com>
2024-05-02 01:26:02 +0300

    Update chal.py

--- Session1/modularinverse/chal.py
+++ Session1/modularinverse/chal.py
@@ -1,1 +1,13 @@
-## psst
+from calendar import c
+import random
+from Crypto.Util.number import getPrime,getRandomInteger,bytes_to_long
+from secret import flag 
+p = getPrime(512)
+
+random = getRandomInteger(512) % p
+coeff = pow(random,-1,p)
+flag = bytes_to_long(flag)
+ct = coeff * flag % p
+print(f'p = {p}')
+print(f'ct = {ct}')
+print(f'random = {random}')
